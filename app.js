4fe3d53d94401fc7ff7aef5e11a7dff1f9a7fa80
Michael Brown <fido@Michaels-MacBook-Pro-6.local>
2015-11-04 14:35:56 -0500

    adding readme.md, permuation successful

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+/**
+ * Permutation of array of strings
+ * By: Michael Brown
+ * 11/03/2015
+ */
+
 var fs = require('fs'),
     pathToFile = process.argv[2],
     usedLetters = [],
     response = [];
 
-
-var arrayText = fs.readFileSync(pathToFile).toString().split(/\n/).reverse();
+// Read file that is passed as third argument from command line. 
+var arrayText = fs.readFileSync(pathToFile).toString().split(/\n/);
 
 iterate(arrayText);
 
+/**
+ * @param  {array}
+ * @return {array}
+ * Execute permutation on given string. 
+ */
 function permutate (input) {
   var i, letter;
 
@@ -18,19 +29,26 @@ function permutate (input) {
     if (input.length == 0) {
       response.push(usedLetters.join(''));
     }
+
     permutate(input);
     input.splice(i, 0, letter);
     usedLetters.pop();
   }
-
   return response;
 }
 
+/**
+ * @param  {array}
+ * @return {String}
+ * Iterate over array of strings in file and call purmutate()
+ */
 function iterate (a) {
-  for (var i = 0; i < a.length; i++) {
-    response.push(permutate(a[i].split('')));
-  };
   
-  console.log(response);
-  return response.sort(); 
+  for (var i = 0; i < a.length; i++) {
+    var string = a[i].split('');
+    response.push(permutate(string));
+    response.pop();
+  };
+
+  return response.sort().join(); 
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Permutation Recursion
+##### By: Michael Brown
+================================================
+
+### Useage
+```
+git clone git@github.com:micha3lbrown/permutations.git
+cd permutation
+node app.js string.txt
+```
