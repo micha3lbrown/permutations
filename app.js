e7b239d4221c0c3fca9afbf5747871e695ab1fec
Michael Brown <fido@Michaels-MacBook-Pro-6.local>
2015-11-04 13:59:21 -0500

    Circular behavior

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     pathToFile = process.argv[2],
-    usedLetters = [];
+    usedLetters = [],
+    response = [];
 
 
 var arrayText = fs.readFileSync(pathToFile).toString().split(/\n/).reverse();
@@ -9,41 +10,27 @@ iterate(arrayText);
 
 function permutate (input) {
   var i, letter;
-  var partial = [];
 
   for (i = 0; i < input.length; i++) {
     letter = input.splice(i,1)[0];
-    console.log(i, letter);
     usedLetters.push(letter);
 
     if (input.length == 0) {
-      partial.push(usedLetters.slice());
+      response.push(usedLetters.join(''));
     }
     permutate(input);
     input.splice(i, 0, letter);
     usedLetters.pop();
   }
 
-  return partial;
+  return response;
 }
 
 function iterate (a) {
-  var response = [];
-
   for (var i = 0; i < a.length; i++) {
     response.push(permutate(a[i].split('')));
   };
-   console.log(response);
+  
+  console.log(response);
+  return response.sort(); 
 }
-
-// for (i = 0; i < input.length; i++) {
-  //   ch = input.splice(i, 1)[0]; /get the first char
-  //   usedChars.push(ch); /push char to usedChars
-  //   if (input.length == 0) { if input is empty push all usedChars
-  //     permArr.push(usedChars.slice());
-  //   }
-  //   permutate(input);
-  //   input.splice(i, 0, ch);
-  //   usedChars.pop();
-  // }
-  // return permArr
